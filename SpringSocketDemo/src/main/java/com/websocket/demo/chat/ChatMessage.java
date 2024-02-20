2cd16177ba7bd68410ccc9fdd1739ad10cf57880
tigerlab <tiggers12@hotmail.com>
2024-02-20 20:45:58 +0700

    fix page

--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
@@ -3,12 +3,22 @@ package com.websocket.demo.chat;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Builder
+@Setter
 public class ChatMessage {
     private String content;
     private String timestamp;
     private String sender;
     private MessageType type;
+    private int onlineUsersCount;
+    public static int Count;
+    public static int addUser(){
+        return ++Count;
+    }
+    public static int deleteUser(){
+        return --Count;
+    }
 }
